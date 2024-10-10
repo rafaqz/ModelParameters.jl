85e0795252aa4b7e997d923d0738c737f84516ec
Rafael Schouten <rafaelschouten@gmail.com>
2024-10-11 00:11:06 +0200

    better MakieModel docs

--- src/makie.jl
+++ src/makie.jl
@@ -1,66 +1,71 @@
 """
     MakieModel(f, model)
 
-An [`AbstractModel`](@ref) that generates its own Interact.jl interface.
+An [`AbstractModel`](@ref) that generates its own Makie.jl interface.
 Each model [`Param`](@ref) has a slider generated for it to update the model.
-After any slider updates the user-defined function `f` is passed the updated
-model to generate a new output - anything that will display in a WebIO.jl
-node, like a Plots.jl plot
+Function `f` is passed a `Makie.GridLayout` to plot into and an `Observables.Observable`
+that holds the model object stripped of `Param`s, with the values of the sliders in 
+the interface. After any slider updates the `Observable` is updated.
 
-After any slider changes, the parent model is updated, so that `parent(model)`
-will return it's latest state.
+Function `f` only runs once, on initialisation. In it `Makie.lift` should be used on the model
+to create an `Observable` which can be plotted into one or many `Makie.Axis` created in
+the `GridLayout`.
 
 ## Arguments
 
-- `f`: a function that take the model object as an argument, but with
-  `Param` fields replaced with their values. Usually a `do` block.
+- `f`: a function that take a `GridLayout` and model object wrapped as an `Observable` as arguments.
 - `model`: any object with [`Param`](@ref)s objects in some fields.
-
-## Param fields
-
-`Param` objects in the model need to include a `range` or `bounds`
-field to define the slider range - holding an `AbsractRange` or `NTuple{2}`, respectively.
-
-Optionally, the `Param`s can also include:
-- A `label` field to use instead of field names
-- A `desciption` field to use in mouse hover text
 
 ## Keyword Arguments
 
 - `title`: `""` set a window title, if you need it.
-- `throttle`: `0.1`. Slider throttle, in seconds. Adjust to improve performance.
+- `slider_kw`: An optional `NamedTuple` of keywords to pass to all sliders.
+- `ncolumns`: Group sliders in `n` columns, `1` by default.
 - `figure`: An optional Makie `Figure`.
-- `axis`: An optional Makie `Axis`.
-- `layout`: `vbox`. This can be any three-argument function that will combine title,
-  output, and sliders (all WebIO nodes) into a combined WebIO node. You can use this
-  method to do any additional layout you need.
+- `layout`: An optional Makie `GridLayout` that gets passed to `f`
+
+## Param fields
+
+`Param` objects in the model can include keywords:
+
+- `label`: field to use instead of field names
+- `range`: an `AbsractRange` of slider positions
+- `bounds`: an `NTuple{2}` for min and max of slider ranges, 
+    if `range` is not available.
+
+Withouth `range` or `bounds` the range will be guessed from `val`.
 
 ## Example
 
-This is a simple example where the model is a `NamedTuple`, adapted from
-an Interact.jl example. It will display in atoms plot pane:
+This is a simple example where the model is a `NamedTuple` that changes
+the color patterns of 
 
 ```julia
 using ModelParameters, GLMakie, CSV
 
 # Define some parameters
-p = (; 
+model = (; 
     noise=Param(0.5, bounds=(0.0, 1.0), label="Noise"),
     color=Param(0.5, bounds=(0.0, 1.0), label="Color"),
 )
 
+# Define a function that generates a random array from our model `m`
+randarray(m) = max.(min.((rand(10, 10) .- 0.5) .* m.noise .+ m.color, 1.0), 0.0)
+
 # Make an interactive model 
-m = MakieModel(p) do layout, model
-    A = lift(model) do m
-        max.(min.((rand(10, 10) .- 0.5) .* m.noise .+ m.color, 1.0), 0.0)
-    end
+mm = MakieModel(model; ncolumns=2) do layout, model_obs
+    # `model_obs` is our model with `Params` stripped and wrapped as an `Observable`.
+    # We can `lift` it to run `f` and update a new 
+    x = lift(randarray, model_obs)
+    # Define an axis to plot into
     ax = Axis(layout[1, 1])
-    heatmap!(ax, A; colorrange=(0, 1))
+    # And plot a heatmap of the output of `f`
+    heatmap!(ax, x; colorrange=(0, 1))
 end
 
 # We can save the parameters set in the interface
 # to anything Tables.jl compatible, like csv
-CSV.write("modelparams.csv", m)
+CSV.write("modelparams.csv", mm)
 ```
 """
 mutable struct MakieModel{F} <: AbstractModel
